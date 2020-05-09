6d5d116deb791b8b4d5cd240f357c69f8fea3009
Jyoti Singh <jyoti.singhd102@gmail.com>
2020-05-09 11:21:57 +0530

    Initital Commit

---
+++ src/test/java/com/techknow/ssv/ServerSideValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techknow.ssv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
